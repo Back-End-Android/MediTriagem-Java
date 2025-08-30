2b141c0f0dff5c567dc2115f6dde2526e3bc8f7d
✨Bruna ✨ <163605337+Bru67@users.noreply.github.com>
2025-08-30 16:05:57 -0300

    Hospedando Projeto MediTriagem no Github

---
+++ src/main/java/pucpr/meditriagem/project/MediTriagemApplication.java
@@ -0,0 +1,13 @@
+package pucpr.meditriagem.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediTriagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediTriagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pucpr/meditriagem/project/MediTriagemApplicationTests.java
@@ -0,0 +1,13 @@
+package pucpr.meditriagem.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediTriagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
