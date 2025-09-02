7a454fa7f4efd07a7845ffd907e34711b34918d6
Bruna Carnelossi <bruna.carnelossi@pucpr.edu.br>
2025-09-01 21:26:40 -0300

    Inicializando End-Points e Validações, Alocando Dependências

---
+++ src/main/java/pucpr/meditriagem/project/dto/UserDTO.java
@@ -0,0 +1,23 @@
+package pucpr.meditriagem.project.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class UserDTO {
+
+    private Integer id;
+
+    @NotNull(message = "{error.nome.notnull}")
+    @NotBlank(message = "{error.nome.notblank}")
+    private String nome;
+
+    @Email(message = "{error.email.invalid}")
+    @NotBlank(message = "{error.email.notblank}")
+    private String email;
+
+}
